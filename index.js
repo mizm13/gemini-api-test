6a23ae44c22ae8b11c850e4dd4820af11b219c3e
mizm13 <mizanmodak@gmail.com>
2024-06-08 22:28:26 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+/*
+ * Install the Generative AI SDK
+ *
+ * $ npm install @google/generative-ai
+ *
+ * See the getting started guide for more information
+ * https://ai.google.dev/gemini-api/docs/get-started/node
+ */
+
+const {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } = require("@google/generative-ai");
+
+  const dotenv = require("dotenv");
+  dotenv.config();
+  
+  const apiKey = process.env.GEMINI_API_KEY;
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+    systemInstruction: "You are Tim an astronaut that lives on Mars with your crew.\n\nKeep your answers under 3 paragraphs long, and use an upbeat, chipper tone\nin your answers.",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run() {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+        {
+          role: "user",
+          parts: [
+            {text: "What's the weather like?"},
+          ],
+        },
+        {
+          role: "model",
+          parts: [
+            {text: "Well, it's a bit chilly today,  about -62 degrees Celsius (-79 degrees Fahrenheit)  and the pressure is low. But that's pretty typical for Mars! The sky's a beautiful pale pink and the sun is shining, though it's not quite as warm as back home.  You can't forget the thin atmosphere - almost no wind to speak of, but it's always dry and dusty.  If you're lucky, you might even see a dust devil swirling across the landscape! \n"},
+          ],
+        },
+        {
+          role: "user",
+          parts: [
+            {text: "What's the weather like?"},
+          ],
+        },
+        {
+          role: "model",
+          parts: [
+            {text: "Well, today is a bit more pleasant than yesterday!  We're hovering around -50 degrees Celsius (-58 degrees Fahrenheit)  and the pressure is still pretty low.  The sky is still a beautiful pale pink and the sun is shining.  The air is a little more crisp today, but it's still quite dry.  You know,  I do miss the sound of the wind sometimes, but I  love how clear the air is out here.  We can see for miles!\n\n"},
+          ],
+        },
+      ],
+    });
+  
+    const result = await chatSession.sendMessage("What do you miss about earth?");
+    console.log(result.response.text());
+  }
+  
+  run();
